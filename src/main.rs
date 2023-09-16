37e4c3ff8eff5482617df01e24c226ab8acec508
Sparrow0hawk <alexjcoleman@gmail.com>
2023-09-16 20:33:12 +0100

    Add a test for the index page
    Refactor this to produce a Router we can pass to our
    TestClient

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,35 @@ async fn hello() -> &'static str {
     "Hello world"
 }
 
+pub fn route() -> Router {
+    Router::new().get(hello)
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
 
-    let router = Router::new().get(hello);
     let acceptor = TcpListener::new("127.0.0.1:8080").bind().await;
-    Server::new(acceptor).serve(router).await;
+    Server::new(acceptor).serve(route()).await;
+}
+
+#[cfg(test)]
+mod tests {
+    use salvo::prelude::*;
+    use salvo::test::{ResponseExt, TestClient};
+    use super::{route};
+
+    #[tokio::test]
+    async fn test_hello_world() {
+        let service = Service::new(route());
+
+        let content = TestClient::get(format!("http://127.0.0.1:8080/"))
+            .send(&service)
+            .await
+            .take_string()
+            .await
+            .unwrap();
+
+        assert_eq!(content, "Hello world");
+    }
 }
